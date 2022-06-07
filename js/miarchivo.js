6f9deb100e110cdefb1e6b77ba12315ac46e82ed
Ignacio Herner <ignah71@gmail.com>
2022-06-07 15:41:46 -0300

    Trabajo practico localStorage

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -128,3 +128,19 @@ class Productos {
 
     alert("Eres menor de edad");
 }
+
+// Forms localStorage
+
+function guardarDatos(){
+  localStorage.nombre = document.getElementById("nombre").value;
+  localStorage.password = document.getElementById("password").value;
+ }
+ 
+ function recuperarDatos(){
+  if ((localStorage.nombre != undefined) && (localStorage.password != undefined)) {
+   document.getElementById("datos").innerHTML = "Nombre: " + localStorage.nombre + "<br/> Password: " + localStorage.password;
+  }
+  else{
+   document.getElementById("datos").innerHTML = "No has introducido tu nombre y tu password";
+  }
+ }
